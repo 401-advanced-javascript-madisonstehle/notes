c32de7ab255c846f5d9f1cae8ceda323b4b910ff
Madison Stehle <mnstehle3@gmail.com>
2020-03-18 18:28:31 -0700

    building out constructor and checks for action/payload

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict';
 
-console.log('i am here!');
+// console.log('i am here!');
+
+// ========== MODULES ==========
+const Input = require('./lib/input.js');
+
+// ========== PROCESS TERMINAL ARGUMENTS ==========
+let parsedInput = new Input(process.argv.slice(2));
