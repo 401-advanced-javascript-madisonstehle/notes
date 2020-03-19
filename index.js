ffd614c7afbf2ed6747a121ed970209f59c06fc5
Madison Stehle <mnstehle3@gmail.com>
2020-03-19 09:19:11 -0700

    scaffolded notes, following demo

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 // ========== MODULES ==========
 const Input = require('./lib/input.js');
+const Notes = require('./lib/notes.js');
 
 // ========== PROCESS TERMINAL ARGUMENTS ==========
 let parsedInput = new Input(process.argv.slice(2));
+let note = new Notes(parsedInput);
